d7e1d18b16f3430464d691ad06bd14f9ea3b5bbd
Bruno Groppo <bmgroppo@gmail.com>
2025-05-13 15:33:00 -0300

    primeiro

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import './App.css'
+
+function App() {
+ 
+  return (
+    <>
+      <header>
+        <h1>B. Miranda</h1>
+        <p>Estudante de Tecnologia da Informação</p>
+      </header>
+      <main>
+        <h2>Sobre mim</h2>
+        <p>Lorem, ipsum dolor sit amet <strong>consectetur</strong> adipisicing elit. 
+          Animi velit in voluptatem, sint cum veritatis ipsam dolor consequatur quia, 
+          ullam necessitatibus enim doloremque quidem maiores 
+          quam maxime natus distinctio quos!</p>
+
+          <p>Lorem, ipsum dolor sit amet <strong>consectetur</strong> adipisicing elit. 
+          Animi velit in voluptatem, sint cum veritatis ipsam dolor consequatur quia, 
+          ullam necessitatibus enim doloremque quidem maiores 
+          quam maxime natus distinctio quos!</p>
+
+          <h3>Habilidades Principais</h3>
+          <ul>
+            <li>Montagem e configuração de PC</li>
+            <li>Redes e conectividades</li>
+            <li>Sistemas Operacionais (windows, Linux e MS-DOS</li>
+            <li>Didática Técnica para cursos e certificações</li>
+          </ul>
+      </main>
+      <footer>
+        &copy; 2025 - Bruno Groppo - Todos os direitos reservados
+      </footer>
+    </>
+  )
+}
+
+export default App
